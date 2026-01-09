bf2a862b49c4cdb44d9b0a20b6c698d1d5d4e623
andrewdp <andrew.depeiza@cra-arc.gc.ca>
2026-01-09 10:11:53 -0500

    listener test

--- source/scripts/git-dev-options-e.js
+++ source/scripts/git-dev-options-e.js
@@ -44,7 +44,7 @@ let notedLinksArr, pageOrigin,
         }
     };
 
-document.addEventListener("DOMContentLoaded", function initDevOpts(event) {
+document.addEventListener("load", function initDevOpts(event) {
     let devOptionsLocStore = null, 
         editStartContent = "";
 
